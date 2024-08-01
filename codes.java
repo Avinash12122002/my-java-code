a5857fef591d60199209fbc49b4fa94d38ba88db
Avinash12122002 <akak2805034@gmail.com>
2024-08-01 17:13:07 +0530

    java codes

--- codes.java
+++ codes.java
@@ -1,2 +1,184 @@
 
 
+// import java.util.Scanner;
+
+// public class first {
+//     public static void function(){
+//             System.out.println("Hello");
+//            }
+//            public static void name(String name){
+//            System.out.println(name);
+//         }
+    
+//         public static void numbers(int n1,int n2){
+//             System.out.println(n1+n2);
+//         }
+
+//     public static void main(String[] args) {
+//         System.out.println("avinash");
+//         String name = "avinash bhai";
+//         String add = "delhi";
+//         int age = 30;
+//         String friend = name;
+//         int phone =935449761 ;
+//         long mobile = 9354497616456151L;
+//         float pi = 3.14F;
+//         String adding = name+" "+add;;
+//         System.out.println(name);
+//         System.out.println(add);
+//         System.out.println(age); 
+//         System.out.println(friend);
+//         System.out.println(adding);
+//         String[] name= new String[4];
+//         name[0]="Rahul";
+//         name[1]="Avinash";
+//         name[2]="aryaman";
+//         name[3]= "harsh";
+//         System.out.println(name[2]);
+//         int[] array = new int[4];
+//         array[0] = 10;
+//         array[1] = 20;
+//         array[2] = 30;
+//         array[3] = 40;
+//         System.out.println(array[2]);
+//          String Student_name ="avinash";
+//         int age = 21;
+//         String course = "mca";
+//         float fees = 100000.00F;
+//         System.out.println("Student name is " + Student_name);
+//         System.out.println("student age is " + age );
+//         System.out.println("student course is " + course);
+//         System.out.println("student fees is " + fees); 
+
+//         // Area of Triangle :
+//         int base = 10;
+//         int hight = 20;
+//         int area = base*hight;
+//         System.out.println("Area of Triangle is " + area/2);
+//         String name = "Avinash";
+//         System.out.println(name.toUpperCase()); 
+//         System.out.println(name .toLowerCase());
+//         System.out.println(Math.max(20, 30));
+//         System.out.println(Math.sqrt(62));
+//         int a = 30;
+//         int b = 20;
+        
+//         System.out.println(a>b);
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter your age");
+//         int age = sc.nextInt();
+//         Scanner sc1 = new Scanner(System.in);
+//         System.out.println("Enter your name"   );
+//         String name = sc1.nextLine();
+//         System.out.println("your age is "+age);
+//         System.out.println("your name is "+name);
+//         Scanner sc2 = new Scanner(System.in);
+//         System.out.println("Enter your friend name");
+//         String friend_name  = sc2.nextLine();
+//         System.out.println("your friend name is "+ friend_name);
+    
+//         Scanner n1 = new Scanner(System.in);
+//         System.out.println("Enter a number");
+//         int num1 = n1.nextInt();
+//         System.out.println("Enter a second number");
+//         int num2 = n1.nextInt();
+//         int sum = num1+num2;
+//         Boolean bool = num1>num2;
+//         System.out.println("The Sum Of Two Number Is "+sum);
+//         System.out.println("The Number Is Less Than "+bool);
+        
+//        int a = 10;
+//        int b = 20;
+//        if (a>b) {
+//         System.out.println("a is greatest");
+//        }
+//        else if (b==a) {
+//         System.out.println("a and b are equal");
+//        }
+//        else {
+//         System.out.println("b is greatest");
+//        }
+
+//     Scanner num = new Scanner(System.in);
+//     System.out.println("Enter a first number ");
+//     int a = num.nextInt();
+//     System.out.println("Enter a seconed number ");
+//     int b = num.nextInt();
+    
+//     if (a > b){
+//         System.out.println("a is greater than b ");
+//     }
+//     else if (a < b) {
+//         System.out.println("b is greater than a ");
+//     }
+//     else {
+//         System.out.println("a and b are equal ");
+//     }
+//         Scanner day = new Scanner(System.in);
+//     System.out.println("Enter day number");
+//     int days = day.nextInt();
+
+//     switch (days) {
+//         case 1 :
+//         System.out.println("Monday");
+//         break;
+//         case 2:
+//         System.out.println("Tuesday");
+//         break;
+//         case 3 :
+//         System.out.println("Wednesday");
+//         break;
+//         case 4 :
+//         System.out.println("Thursday");
+//         break;
+//         case 5 :
+//         System.out.println("Friday");
+//         break;
+//         case 6 :
+//         System.out.println("Saturday");
+//         break;
+//         case 7 :
+//         System.out.println("Sunday");    
+//         break;   
+//     }
+//          for(int i =1; i<=100;i++){
+//             System.out.println(i);
+//         }
+//          int i = 1;
+//          while (i<=100) {
+//             System.out.println(i);
+//             i++;
+//          }
+//          int k= 1;
+//          do{
+//             System.out.println(k);
+//             k++;
+//          }
+//          while(k<=100);
+
+//         Scanner loop = new Scanner(System.in);
+//         int i = 0;
+//         do {
+//             System.out.println("You enter a number ");
+//             i = loop.nextInt();
+//             System.out.print("You enter a number");
+//             System.out.println(i);
+//         }
+//         while(i>=0);
+//         System.out.println("The end");
+
+//         int [] marks = {85,98,95};
+//    try{
+//    System.out.println(marks[5]);
+//    }
+//    catch(Exception ex){
+//     System.out.println("Exception caught");
+//    }
+//    System.out.println(marks[2]);
+//    function();
+//     name("avinash");
+//     numbers(5,6);
+
+        
+//     }
+// }  
